383b1261c1e22631a5c0e744d68858b73f36f9cf
BohdanHoriachyi <horiachyi.bohdan@chnu.edu.ua>
2024-02-26 10:27:24 +0200

    aughtask

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -1,38 +1,107 @@
-﻿// OOPLab2Tpl.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream> 
 using namespace std;
-#include "Tasks.h"
-#include "Examples.h"
-
-
-/// @brief 
-/// @return 
-int main()
-{
-    cout << "OOP. Template for laboratory work #2.\n";
-
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = cin.get();
-        
-        cin.get();
-
-        switch (ch) {
-        case '1': task1();   break;
-        case '2': task1();   break;
-        case '3': task1();   break;
-        case '4': task1();   break;
-		case '5':  Examples(); break;
-		case '6': return 0;
-	    }
-        cout << " Press any key and enter\n";
-        ch = cin.get();
-    } while (ch != '6');
-
+int main() {
+    int a, b, c, d;
+    int x, y;
+    std::cin >> a >> b >> c >> d;
+    x = ((((a << 4) + a) + ((c << 8) + (c << 7) - (c << 6) - (c << 3))) >> 5) - ((b << 7) - (b << 3)) + ((d << 7) - d);
+    y = (17 * a + 312 * c) / 32 - b * 120 + d * 127;
+    std::cout << x << " " << y;
     return 0;
 }
+/*#include <iostream>
+#include <bitset>
+#include <string>
+#include <fstream>
 
+// Функція для шифрування
+void encrypt(std::string text[8], unsigned short rez[8][8]) {
+    short i, j, l, m;
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            unsigned char c;
+            unsigned short r, t, b;
+            r = 0;
+            c = text[i][j];
+
+            r |= i; // Номер рядка
+            r |= j << 3; // Позиція символа в рядку
+
+            t = 1;
+            b = 0;
+            for (l = 0; l < 7; l++) {
+                if (r & t) {
+                    if (b == 0) b = 1; else b = 0;
+                }
+                t <<= 1;
+            }
+            r |= b << 6; // Біт парності
+
+            r |= ((int)c << 7); // ASCII-код
+
+            t = 1;
+            b = 0;
+            for (m = 7; m < 16; m++) {
+                if (r & t) {
+                    if (b == 0) b = 1; else b = 0;
+                }
+                t <<= 1;
+            }
+            r |= b << 15; // Біт парності
+            rez[i][j] = r;
+        }
+    }
+}
+
+// Функція для дешифрування
+void decrypt(unsigned short rez[8][8], std::string decryptedText[8]) {
+    short i, j;
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            unsigned short r = rez[i][j];
+            unsigned char ascii = r >> 7; // Відокремлюємо ASCII-код
+            decryptedText[i][j] = ascii;
+        }
+    }
+}
+
+int main() {
+    std::string text[8];
+    unsigned short rez[8][8];
+
+    for (int i = 0; i < 8; i++) {
+        std::cout << "vvedit ryadok " << i + 1 << ": ";
+        std::getline(std::cin, text[i]);
+    }
+
+    for (int i = 0; i < 8; i++) {
+        while (text[i].length() < 8) {
+            text[i] += ' ';
+        }
+    }
+
+    encrypt(text, rez);
+
+    std::ofstream inf("binary.dat", std::ios::binary);
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            std::bitset<16> binary(rez[i][j]);
+            std::cout << binary << std::endl;
+            inf.write(reinterpret_cast<const char*>(&rez[i][j]), sizeof(rez[i][j]));
+        }
+    }
+    inf.close();
+
+    // Дешифрування (при потребі)
+    std::string decryptedText[8];
+    decrypt(rez, decryptedText);
+
+    std::cout << "deshifrovaniy text: " << std::endl;
+    for (int i = 0; i < 8; i++) {
+        std::cout << decryptedText[i] << std::endl;
+    }
+
+    return 0;
+}*/
